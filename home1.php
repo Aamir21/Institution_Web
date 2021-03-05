714a181f34a9c131f44e6602e8bee817f50d7a39
Md. Aamir Sohail <42406623+Aamir21@users.noreply.github.com>
2021-03-05 17:38:46 +0530

    Add files via upload

---
+++ home1.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        
+        <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Advent+Pro|Alfa+Slab+One|Anton|Archivo+Narrow|Baloo+2|Baloo+Chettan+2|Bellota|BenchNine|Caveat|Cinzel|Comfortaa|Concert+One|Cookie|Damion|Dancing+Script|Dosis|Electrolize|Fredoka+One|Great+Vibes|Indie+Flower|Inter|Josefin+Sans|Josefin+Slab|Kaushan+Script|Lato|Lobster|Luckiest+Guy|Monoton|Montserrat|Open+Sans|Oswald|Oxanium|PT+Sans+Caption|Pacifico|Parisienne|Passion+One|Poiret+One|Poppins|Quattrocento+Sans|Raleway|Righteous|Roboto+Mono|Sacramento|Satisfy|Sen|Source+Sans+Pro|Spartan|Titillium+Web|Ubuntu|Ultra|Yellowtail&display=swap" rel="stylesheet">
+            
+            
+            
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        
+        <meta charset="utg-8">  
+        
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+        
+        
+        <style type="text/css">
+            
+            .seg1{
+                height: 122px;
+                background-color: #F8F8F8;                  /*  #DE0950  */
+                
+                
+            }
+            
+            
+            #logo{
+                width:170px;
+                height: 100px;
+                background-color: transparent;
+                background-image: url(logo1.png);
+                background-size: 140px;
+                margin-top: 10px;
+                background-repeat: no-repeat;
+            }
+            
+            .seg1_2{
+                height: 122px;
+                background-color:#F8F8F8; 
+                padding-top: 90px;
+                
+            }
+            
+            .seg1_2 ul{
+                background-color: lightgreen;
+                
+            }
+            
+            .seg1_2 ul li{
+                float: left;
+                margin-left: 5px;
+                list-style-type: none;
+                font-family: Poppins;
+                width: 130px;
+                height: 27px;
+                padding-top: 2px;
+                padding-bottom: 5px;
+                background:linear-gradient(90deg,darkorange, crimson);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                text-align: center;
+                transition: all 0.2s ease-out;
+                
+            }
+            .seg1_2 ul li:hover{
+                border: solid 2px;
+                cursor: pointer;
+                border:linear-gradient(90deg,darkorange, crimson);
+                color: crimson;
+                border-radius: 3px;
+                padding-top: 2px;
+                padding-bottom: 5px;
+                height: 30px;
+                transition: all 0.2s ease-out;
+            }
+            
+           
+            
+            /*Seg2*/
+            
+            
+            .seg2{
+                height: 900px;
+                background-color: #F8F8F8;
+            }
+            
+            .seg2 blockquote{
+                width: 400px;
+                font-family: Poppins;
+               
+            }
+            .seg2 blockquote h1 b{
+                font-size: 47px;
+                 background: linear-gradient(90deg, darkorange, crimson);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+            
+            
+            svg{
+                opacity: 0.5;
+            }
+            
+            .seg2 p{
+                
+                font-family: Poppins;
+                font-size: 25px;
+                 background: linear-gradient(90deg, seagreen, green);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                
+                
+            }
+            
+            #carouselExampleFade{
+                height: 400px;
+                
+            }
+            
+            .active{
+                height: 400px;
+            }
+            
+            .carousel-item{
+                height: 400px;
+            }
+        
+            
+            .section1{
+                height: 300px;
+                background:-webkit-linear-gradient(top, #F8F8F8 60%, grey 40%);
+                
+                
+            }
+            
+            .section1 div{
+                height: 250px;
+                background-color: white;
+                border: solid thin gray;
+                float: left;
+                margin-left: 80px;
+                padding-left: 0px;
+                margin-top: 20px;
+                box-shadow: 0 0 6px grey;
+                border-radius: 4px;
+                opacity: 0.9;
+                border: solid thick crimson;
+                
+                border-bottom: 0;
+                border-left: 0;
+                border-right: 0;
+                
+            }
+            
+            .section1 div h2{
+                width: 200px;
+                height: 50px;
+                background-color: transparent;
+                font-family: Poppins;
+                font-size: 22px;
+                text-align: center;
+                padding-left: 10px;
+                margin-left: 50px;
+                padding-top: 9px;
+                padding-right: 20px;
+                font-weight: bold;
+                
+                
+                
+            }
+            .section1 div p{
+                font-family: Josefin Slab;
+                font-size: 13px;
+                font-weight: bold;
+                
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            /*media query*/
+            
+            @media only screen and (max-width:768px){
+                
+                #headtext1{
+                    display: none;
+                }
+                
+                
+            }
+            
+            
+            
+            
+        
+        </style>
+    
+    
+    </head>
+    
+    
+    <body>
+        
+        <div class="container-fluid">
+        
+        
+            <div class="row">                           <!--ROW1-->
+            
+                <div class="col-md-3 seg1">
+                    
+                    <div id="logo">                     <!--!logo-->
+                        
+                    </div>
+                    
+                </div>
+                
+                <div class="col-md-9 seg1_2">
+                    
+                    <ul class="col-md-12">
+                        <li>Home</li>
+                        <li>Brochure</li>
+                        <li>About</li>
+                        <li>Contacts</li>
+                    </ul>
+                
+                </div>
+                
+            </div>
+            
+            <div class="row">                           <!--ROW2-->
+            
+                <div class="col-md-12 seg2">
+                    
+                    
+                    <!--Carousel-->
+              
+                        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                          <div class="carousel-inner">
+                            <div class="carousel-item active">
+                              <img class="d-block w-100" src="Matteo-Catanese-photo-1518391846015-55a9cc003b25.jpg" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                              <img class="d-block w-100" src="odessa-ukraina-nacionalnyy.jpg" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                              <img class="d-block w-100" src="physical_science.png" alt="Third slide">
+                            </div>
+                          </div>
+                          <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                          <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </div>
+                    
+                    
+                    
+                    <blockquote><h1><b>ICT University,</b> Asian School of Art and Cinema!</h1></blockquote>
+                    
+                    
+                    
+                    <svg id="Layer_1" data-name="Layer 1"  viewBox="0 0 679 111"><defs><linearGradient id="linear-gradient" x1="68" y1="273.5" x2="747" y2="273.5" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#bdccd4"/><stop offset="1" stop-color="#e6e6e6"/></linearGradient></defs><title>reel1</title><path d="M747,310.38V315a3.12,3.12,0,0,0-3,3.11v3.76a3.12,3.12,0,0,0,3,3.11v4H68v-4h2.88A3.12,3.12,0,0,0,74,321.88v-3.76A3.12,3.12,0,0,0,70.88,315H68V232h1.88A3.12,3.12,0,0,0,73,228.88v-3.76A3.12,3.12,0,0,0,69.88,222H68v-4H747v4h-.88a3.12,3.12,0,0,0-3.12,3.12v3.76a3.12,3.12,0,0,0,3.12,3.12H747v4.62" transform="translate(-68 -218)" style="fill:url(#linear-gradient)"/><rect x="46" y="17" width="95" height="77" rx="12" style="fill:#e9e3ff"/><rect x="182" y="17" width="95" height="77" rx="12" style="fill:#f2f2f2"/><rect x="318" y="17" width="95" height="77" rx="12" style="fill:#f9fff1"/><rect x="454" y="16" width="95" height="77" rx="12" style="fill:#fff"/><path d="M747,310.38a11.89,11.89,0,0,1-6,1.62H670a12,12,0,0,1-12-12V247a12,12,0,0,1,12-12h71a11.89,11.89,0,0,1,6,1.62" transform="translate(-68 -218)" style="fill:#fff"/><rect x="46" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="63" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="80" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="12" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="29" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="97" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="114" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="131" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="148" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="165" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="182" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="199" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="216" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="233" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="250" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="267" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="284" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="301" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="318" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="335" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="352" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="369" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="386" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="403" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="420" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="437" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="454" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="471" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="488" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="505" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="522" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="539" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="556" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="573" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="590" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="607" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="624" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="641" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="658" y="4" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="47" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="64" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="81" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="98" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="115" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="13" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="30" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="132" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="149" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="166" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="183" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="200" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="217" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="234" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="251" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="268" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="285" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="302" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="319" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="336" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="353" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="370" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="387" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="404" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="421" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="438" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="455" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="472" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="489" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="506" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="523" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="540" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="557" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="574" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="591" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="608" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="625" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="642" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/><rect x="659" y="97" width="10" height="10" rx="3.12" style="fill:#fff"/></svg>
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    <p>We are expecting very much responses from the affiliated <br>institutions and and all those students and internees or trainees!<br></p>
+ 
+               
+                
+                </div>
+                
+                <section class="col-md-12 section1">
+                    <div class="col-md-3 sub_sec">
+                        <h2 id="headtext1">Visual Arts</h2>
+                        <p>"Going through so many creative displays in visual forms</p>
+                    </div>
+                    <div class="col-md-3 sub_sec">
+                        <h2 id="headtext2">Mass Communication</h2>
+                        <p>"Working innovatively in people addressing"</p>
+                    </div>
+                    <div class="col-md-3 sub_sec">
+                        <h2 id="headtext3">Drama</h2>
+                        <p>"This is a the best attempt those critical issues"</p>
+                    </div>
+                
+                </section>
+                
+                
+                
+                
+            
+            </div>
+        
+        
+        </div>
+        
+        
+        
+        
+
+
+    </body>
+
+
+</html>
---
+++ tooltip.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        
+        <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Advent+Pro|Alfa+Slab+One|Anton|Archivo+Narrow|Baloo+2|Baloo+Chettan+2|Bellota|BenchNine|Caveat|Cinzel|Comfortaa|Concert+One|Cookie|Damion|Dancing+Script|Dosis|Electrolize|Fredoka+One|Great+Vibes|Indie+Flower|Inter|Josefin+Sans|Josefin+Slab|Kaushan+Script|Lato|Lobster|Luckiest+Guy|Monoton|Montserrat|Open+Sans|Oswald|Oxanium|PT+Sans+Caption|Pacifico|Parisienne|Passion+One|Poiret+One|Poppins|Quattrocento+Sans|Raleway|Righteous|Roboto+Mono|Sacramento|Satisfy|Sen|Source+Sans+Pro|Spartan|Titillium+Web|Ubuntu|Ultra|Yellowtail&display=swap" rel="stylesheet">
+            
+            
+            
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+        <meta charset="utg-8">
+    
+        <meta name="viewport" width="device-width , initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+        
+        
+        
+        <style type="text/css">
+        
+            a{
+                margin-left: 150px;
+                margin-top: 200px;
+            }
+        
+        
+        
+        </style>
+        
+        
+        
+        
+        
+        
+    </head>
+    
+    
+    <body>
+        
+        <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-toggle="popover" data-trigger="focus" title="Dismissible popover" data-content="And here's some amazing content. It's very engaging. Right?">Dismissible popover</a>
+        
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    </body>
+    
+</html>
+    
+    
+    
